cbc620a238d3159ca3bbf34c15a6cbc6f9d9cc74
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-16 22:42:34 +0600

    problem 2 solved

--- src/solution.ts
+++ src/solution.ts
@@ -8,3 +8,14 @@ const formatValue = (input: string | number | boolean) => {
         return !input
     }
 }
+
+const getLength = (value: string | any[]): number => {
+    if (typeof value === 'string') {
+        return value.length
+    } else if (Array.isArray(value)) {
+        return value.length
+    } else {
+        return 0
+    }
+}
+
