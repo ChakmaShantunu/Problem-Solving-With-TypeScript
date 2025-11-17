819d9b3ee018cc8e15e01c4063eafef8da59ca63
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-17 10:24:13 +0600

    problem 5 add filterActiveUsers function for active user filtering solved

--- src/solution.ts
+++ src/solution.ts
@@ -47,8 +47,28 @@ const filterByRating = (books: Item[]): Item[] => {
 }
 
 const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
 ];
+
+interface Users {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+const filterActiveUsers = (users: Users[]): Users[] => {
+    const filterActiveUsers = users.filter(user => user.isActive === true)
+    return filterActiveUsers
+}
+
+const users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+console.log(filterActiveUsers(users));
 
